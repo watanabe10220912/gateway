c73f535aa63175179843a610b319b93ff329b1e3
watanabe10220912 <watanabe10220912@gmail.com>
2021-10-16 15:30:06 +0900

    初期コミット

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <h1>Gen Watanabe</h1>
+        <ul>
+          <li>profile</li>
+          <li>works</li>
+          <li>contact</li>
+        </ul>
+      </body>
     </div>
   );
 }
